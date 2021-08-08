d16c0ce70f76fd6781a459f6e3dc90a5640e0ede
zm <zm@nnsetstzs.com>
2021-08-08 08:18:25 +0800

    three

--- test01/src/test/test01.java
+++ test01/src/test/test01.java
@@ -4,5 +4,6 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("hello pull");
+        System.out.println("hello three");
     }
 }
